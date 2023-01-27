7efc59543fd74caf1fc623ea05d5f2d2925ca262
bjornregnell <bjorn.regnell@cs.lth.se>
2023-01-28 00:03:21 +0100

    fix show string

--- show.scala
+++ show.scala
@@ -1,7 +1,10 @@
 package reqt
 
-import lang.*
-
-extension (a: Any) def show: String = a match
-  case _: Int | Double | Float | Boolean => a.toString
-  case _: String => s"\"$a\""
+extension (a: Any) def show: String = 
+  val s = a.toString
+  a match
+    case _: Int | Double | Boolean => s
+    case _: String => if s.contains('"') || s.contains('\n') then s"\"\"\"$s\"\"\"" else s"\"$s\""
+    case _: Float => s"${s}F"
+    case _: Long => s"${s}L"
+    case _ => s
