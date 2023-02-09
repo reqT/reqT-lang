5ff635445e5a862bab43ff4c3c9f575e42e03a1a
bjornregnell <bjorn.regnell@cs.lth.se>
2023-02-09 21:08:20 +0100

    rename shorten

--- model.scala
+++ model.scala
@@ -12,7 +12,7 @@ final case class Ent(et: EntType, id: String) extends Node
 final case class Attr[T <: Int | String](at: AttrType, value: T) extends Node
 
 final case class Rel(e: Ent, rt: RelType, sub: Model) extends Elem:
-  lazy val expandTopSubnodes: Vector[Rel] = 
+  lazy val expandSubnodes: Vector[Rel] = 
     sub.elems.collect{ case n: Node => n }.map(n => Rel(e, rt, Model(n)))
 
 final case class Model(elems: Vector[Elem]):
