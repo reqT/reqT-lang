71a7e83210fa9c2cba12ac258ee8ef89a9fc2df9
bjornregnell <bjorn.regnell@cs.lth.se>
2023-02-21 21:09:51 +0100

    add tests

--- src/test/scala/TestModelOps.scala
+++ src/test/scala/TestModelOps.scala
@@ -2,5 +2,12 @@ class TestModelOps extends munit.FunSuite:
   import Console.{RED as R, RESET as X}
   import reqt.*
 
-  test("Concat empty models   "):
+  test("Concat empty models        "):
     assert(Model() ++ Model() == Model()) 
+
+  test("Concat non-empty models    "):
+    assert( Model(Req("y"), Prio(1)) ++ Model(Req("y") has (Prio(1), Req("y"))) == 
+            Model(Req("y"), Prio(1), Req("y") has (Prio(1), Req("y"))))
+
+  test("Add entity to empty model  "):
+    assert(Model() + Req("test") == Model(Req("test"))) 
