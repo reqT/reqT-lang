6bca445d2e1fd6c3633098310d45ed9073449038
bjornregnell <bjorn.regnell@cs.lth.se>
2024-02-05 20:15:39 +0100

    fix some tests

--- src/test/scala/TestParser.scala
+++ src/test/scala/TestParser.scala
@@ -4,54 +4,54 @@ class TestParser extends munit.FunSuite:
   import Console.{RED as R, RESET as X}
 
   test("Simple StrAttr   "):
-    assert("Spec  x x x \n y".toModel == Model(Spec("x x x\n y"))) 
+    assert("* Spec  x x x \n y".toModel == Model(Spec("x x x\n y"))) 
 
   test("2 Simple StrAttr "):
-    assert("Spec  x\nSpec y".toModel == Model(Spec("x"),Spec("y"))) 
+    assert("* Spec  x\n* Spec y".toModel == Model(Spec("x"),Spec("y"))) 
 
   test("Multiline StrAttr"):
-    assert("Spec  x x x\n y y y".toModel == Model(Spec("x x x\n y y y"))) 
+    assert("* Spec  x x x\n y y y".toModel == Model(Spec("x x x\n y y y"))) 
 
   test("Simple Non-elem  "):
     assert("xxx\n yyy".toModel == Model(Text("xxx\n yyy"))) 
 
   test("Illegal IntAttr  "):
-    assert("Prio x y z".toModel == Model(Undefined(Prio), Text("x y z"))) 
+    assert("* Prio x y z".toModel == Model(Undefined(Prio), Text("x y z"))) 
 
   test("Simple IntAttr   "):
-    assert("Prio 1".toModel == Model(Prio(1))) 
+    assert("* Prio 1".toModel == Model(Prio(1))) 
 
   test("IntAttr + space  "):
-    assert("Prio 1   ".toModel == Model(Prio(1))) 
+    assert("* Prio 1   ".toModel == Model(Prio(1))) 
 
   test("IntAttr + extra  "):
-    assert("Prio 1 x y ".toModel == Model(Prio(1), Text("x y"))) 
+    assert("* Prio 1 x y ".toModel == Model(Prio(1), Text("x y"))) 
 
   test("Simple Ent       "):
-    assert("Feature  x ".toModel == Model(Feature("x"))) 
+    assert("* Feature  x ".toModel == Model(Feature("x"))) 
 
   test("Simple Ent +extra"):
-    assert("Feature  x y ".toModel == Model(Feature("x y"))) 
+    assert("* Feature  x y ".toModel == Model(Feature("x y"))) 
 
   test("Empty single Rel "):
-    assert("Feature x has".toModel == Model(Feature("x").has()))
+    assert("* Feature x has".toModel == Model(Feature("x").has()))
 
   test("Rel sub1         "): 
-    assert("Feature x has\n Prio 1".toModel == Model(Feature("x").has(Prio(1))))
+    assert("* Feature x has\n * Prio 1".toModel == Model(Feature("x").has(Prio(1))))
   
   test("Rel sub2 outdent "): 
     assert:
-      "Feature x has\n Prio 1\n Req y\nReq z".toModel == 
+      "* Feature x has\n * Prio 1\n * Req y\n* Req z".toModel == 
         Model(Feature("x").has(Prio(1),Req("y")),Req("z"))
 
   test("Illegal id in Rel"): 
     assert:
-      "Feature x y has\n Prio 1\n  Req y\nReq z".toModel == 
+      "* Feature x y has\n * Prio 1\n  * Req y\n* Req z".toModel == 
         Model(Feature("x y") has (Prio(1),Req("y")), Req("z"))
 
   test("Missing id + more"): 
     assert:
-      "Feature    has\n Prio 1\n  Req y\nReq z".toModel == 
+      "* Feature    has\n * Prio 1\n  * Req y\n* Req z".toModel == 
         Model(
           Feature("???") has(Prio(1),Req("y")),
           Req("z")
@@ -59,7 +59,7 @@ class TestParser extends munit.FunSuite:
 
   test("Missing id       "): 
     assert:
-      "Feature\n Prio 1\nReq y\nReq z".toModel == 
+      "* Feature\n * Prio 1\n* Req y\n* Req z".toModel == 
         Model(
           Feature("???") has Prio(1),
           Req("y"),Req("z")
@@ -68,10 +68,10 @@ class TestParser extends munit.FunSuite:
   test("Bad indent       "):
     assert:
       s"""|
-          |Feature x
-          |  Prio 1
-          |   Req y
-          |Req z
+          |* Feature x
+          |  * Prio 1
+          |   * Req y
+          |* Req z
           |
           |""".stripMargin.toModel == 
         Model(
