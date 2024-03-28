f814394046544e750f57aa674e930b20ffbebe2a
bjornregnell <bjorn.regnell@cs.lth.se>
2024-03-28 18:49:49 +0100

    add LatexGen, listing, elem colors

---
+++ src/main/scala/05-LatexGen.scala
@@ -0,0 +1,26 @@
+package reqt
+
+object LatexGen:
+  def defineColors: String = 
+    s"""|%\\usepackage[usenames,dvipsnames,svgnames,table]{xcolor}
+        |\\definecolor{entityColor}{RGB}{0,100,200}
+        |\\definecolor{attributeColor}{RGB}{0,100,50}
+        |\\definecolor{relationColor}{RGB}{160,0,30}
+        |""".stripMargin
+
+  def lstDefineStyle: String = 
+    s"""|\\lstdefinestyle{reqT}{
+        |  %belowcaptionskip=1\\baselineskip,
+        |  breaklines=true,
+        |  %showstringspaces=false,
+        |  showspaces=false,
+        |  %breakatwhitespace=true,
+        |  basicstyle=\\ttfamily\\small,
+        |  emph={${meta.entityNames.mkString{","}}},
+        |  emphstyle=\\bfseries\\color{entityColor},
+        |  emph={[2]${meta.relationNames.mkString{","}}},
+        |  emphstyle={[2]\\bfseries\\color{relationColor}},
+        |  emph={[3]${meta.attributeNames.mkString{","}}},
+        |  emphstyle={[3]\\bfseries\\color{attributeColor}},  
+        |}
+        |""".stripMargin
